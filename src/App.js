4da970a53e778443db471e856885bc4c9c45dffd
LouisVitenskap <70730534+LouisVitenskap@users.noreply.github.com>
2023-10-25 18:39:46 +0200

    Implemented routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import Homepage from "./components/homepage/Homepage";
+import SharedLayout from './components/sharedLayout/SharedLayout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navbar/>    
-      <Homepage/>    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout/>}>
+          <Route index element={<Homepage/>}></Route>
+        </Route>
+      </Routes>          
+    </BrowserRouter>
   );
 }
 
